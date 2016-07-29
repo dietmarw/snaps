0b584fbc9a3f381d09b0ec531d856f9c61464256
Dietmar Winkler <dietmar.winkler@dwe.no>
2016-07-29 14:46:58 +0200

    Adding more paths to get the menus working.

--- ipe/wrapper.sh
+++ ipe/wrapper.sh
@@ -1,5 +1,11 @@
-#! /bin/sh
+#!/bin/sh
 
 export IPELUAPATH=$SNAP/share/ipe/7.2.5/lua/?.lua
+export IPELATEXPATH=$SNAP/usr/bin
+export IPESTYLES=$SNAP/share/ipe/7.2.5/styles
+export IPELETPATH=$SNAP/lib/ipe/7.2.5/ipelets
+export IPEICONDIR=$SNAP/share/ipe/7.2.5/icons
+export IPEDOCDIR=$SNAP/share/ipe/7.2.5/doc
+export IPEDEBUG=1
 
 exec $SNAP/bin/ipe
