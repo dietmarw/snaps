25a3c47d77bce09ad9f72df98687fbad0693f58f
Dietmar Winkler <dietmar.winkler@dwe.no>
2016-08-08 19:48:59 +0200

    This at least gets the tex system to launch
    still problems with finally running pdflatex

--- ipe/wrapper.sh
+++ ipe/wrapper.sh
@@ -7,5 +7,10 @@ export IPELETPATH=$SNAP/lib/ipe/7.2.5/ipelets
 export IPEICONDIR=$SNAP/share/ipe/7.2.5/icons
 export IPEDOCDIR=$SNAP/share/ipe/7.2.5/doc
 export IPEDEBUG=1
+export TEXMFROOT=$SNAP/usr/share/texlive
+export TEXMFHOME=$HOME/texmf
+export TEXMFCONFIG=$HOME/texmf
+export TEXMFVAR=$HOME/texmf
+export PERL5LIB=$SNAP/usr/share/perl:$SNAP/usr/lib/x86_64-linux-gnu/perl
 
-exec $SNAP/bin/ipe
+exec "$@"
