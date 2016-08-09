583ef87e2deab97f7d7cb4a6a73be1049e7613a6
Dietmar Winkler <dietmar.winkler@dwe.no>
2016-08-09 11:54:12 +0200

    Cleaning up the path settings. Still incomplete
    mktexfmt is till not finding the fmtutil.cnf
    So some PATH that makes mktexfmt find it is needed.

--- ipe/wrapper.sh
+++ ipe/wrapper.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-export IPELUAPATH=$SNAP/share/ipe/7.2.5/lua/?.lua
+export LC_ALL=C   # get rid of local warning in perl
+
 export IPELATEXPATH=$SNAP/usr/bin
-export IPESTYLES=$SNAP/share/ipe/7.2.5/styles
-export IPELETPATH=$SNAP/lib/ipe/7.2.5/ipelets
-export IPEICONDIR=$SNAP/share/ipe/7.2.5/icons
 export IPEDOCDIR=$SNAP/share/ipe/7.2.5/doc
+export IPEICONDIR=$SNAP/share/ipe/7.2.5/icons
+export IPELETPATH=$SNAP/lib/ipe/7.2.5/ipelets
+export IPELUAPATH=$SNAP/share/ipe/7.2.5/lua/?.lua
+export IPESTYLES=$SNAP/share/ipe/7.2.5/styles
 export IPEDEBUG=1
+
 export TEXMFROOT=$SNAP/usr/share/texlive
-export TEXMFHOME=$HOME/texmf
+
 export TEXMFCONFIG=$HOME/texmf
-export TEXMFVAR=$HOME/texmf
+export TEXMFHOME=$HOME/texmf
+export TEXMFVAR=$HOME/texmfvar
+
 export PERL5LIB=$SNAP/usr/share/perl:$SNAP/usr/lib/x86_64-linux-gnu/perl
 
 exec "$@"
